cecc14c8bc6211474de1c12ab359bf3baf619a53
PetrYakushev <petr.yakushevmail@mail.ru>
2024-10-31 22:25:58 +0300

    PRIVET FRIENDS ETO MOY CODE

--- sources/tmp_modules/first_module_NN.cpp
+++ sources/tmp_modules/first_module_NN.cpp
@@ -1,3 +1,242 @@
-//
-// Created by nikit on 30.10.2024.
-//
+#include "../../headers/NaturalNumber.h"
+#include <iostream>
+
+Compare NaturalNumber::COM_NN_D(const NaturalNumber& other) const
+{
+    int k1 = Big_int.get_size();
+    int k2 = other.Big_int.get_size(); // количество цифр (разряд) во втором числе
+
+    if (k1 > k2) { //если разряд больше
+        return GREATER;
+    }
+    if (k1 < k2) { // если разряд меньше
+        return LESS;
+    }
+    if (k1 == k2) { // если разряды равены
+
+        int num1 = Big_int.get_last()->value; //первая цифра первого числа
+        int num2 = other.Big_int.get_last()->value;//первая цифра второго числа
+
+        if (num1 > num2) { //сравнение двух старших разрядов
+            return GREATER;
+        }
+        if (num1 < num2) {
+            return LESS;
+        }
+
+        int i = 1;
+
+        Node* next_node1 = Big_int.get_last()->next; //сдвиг на следующий разряд первого числа
+        Node* next_node2 = other.Big_int.get_last()->next;//сдвиг на следующий разряд второго числа
+
+        while (i < k1) { //цикл для сравнения всех разрядов обоих чисел
+
+            next_node1 = next_node1->next;//сдвиг на следующий разряд первого числа
+            next_node2 = next_node2->next; //сдвиг на следующий разряд второго числа
+
+            num1 = next_node1->value; //значение i-го разряда первого числа
+            num2 = next_node2->value; //значение i-го разряда второго числа
+
+            if (num1 > num2) { //сравнение двух разрядов
+                return GREATER;
+            }
+            if (num1 < num2) {
+                return LESS;
+            }
+
+            i++;
+        }
+        return EQUAL; //если числа равны
+    }
+}
+
+bool NaturalNumber::NZER_N_B()
+{
+    if ((Big_int.get_size() == 1) && (Big_int.get_first()->value == 0)) {// проверка числа на 0
+        return false; // если число равно 0, то false
+    }
+
+    return true;
+}
+
+void NaturalNumber::ADD_1N_N()
+{
+    if (Big_int.get_first() ->value == 9) { // если изменение числа должно выйти за пределы одного разряда 
+
+        Big_int.get_first()->value = 0;
+        Node* next_node = Big_int.get_first();
+
+        for (int i = 1; i < Big_int.get_size(); i++) { // проход по числу, пока переход в следующие разряды не прекратится
+            next_node = next_node->next;// перемещение на новый разряд
+            if (next_node->value == 9) {//если следующий разряд также изменяет следующий
+                next_node->value = 0;
+                continue;
+            }
+            next_node->value += 1;
+            break;
+        }
+        if (next_node->value == 0) { // если разряд числа увеличивается
+            Big_int.push_back(1);
+        }
+    }
+    else { // стандартное прибавление единицы
+        Big_int.get_first()->value += 1;
+    }
+}
+
+NaturalNumber& NaturalNumber::ADD_NN_N(const NaturalNumber& other) {
+
+    Node* next_node1 = Big_int.get_first(); //отвечает за сдвиг на следующий разряд первого числа
+    Node* next_node2 = other.Big_int.get_first();//отвечает за сдвиг на следующий разряд второго числа
+    int point = 0; //будет принимать себя излишок, если сумма цифр выйдет за пределы одного разряда
+
+    for (int i = 0; i < other.Big_int.get_size(); i++) {
+        int num1 = next_node1->value;
+        int num2 = next_node2->value + point;
+        if (num1 < num2) {
+            num1 += num2;
+            num1 -= 10;
+            point = 1;
+        }
+        else {
+            num1 += num2;
+            point = 0;
+        }
+        next_node1->value = num1;
+        next_node1->next;
+        next_node2->next;
+    }
+    if ((point == 1) && (next_node1->next == nullptr)) {
+        Big_int.push_back(1);
+    }
+}
+
+NaturalNumber& NaturalNumber::MUL_ND_N(const char c)
+{
+    if (c == 0) { // если умножение на 0
+        Big_int.get_first()->next = nullptr; // отрываем остальную часть числа
+        Big_int.get_first()->value = 0; // число становится 0
+    }
+    if (Big_int.get_first()->value * c >= 10) { // если умножение последней цифры даёт двузначное число
+
+        int desatok = (Big_int.get_first()->value * c) / 10; // десяток от умножения
+        Big_int.get_first()->value = (Big_int.get_first()->value * c) - (desatok * 10); // замена последней цифры
+
+        if (Big_int.get_size() > 1) { //если число не однозначное
+            Node* next_node = Big_int.get_first()->next;//получение следующего разряда
+            if (next_node->value + desatok > 10) { // если сумма второй с конца цифры и десятка произведения даёт двузначное число
+
+                next_node->value = (next_node->value + desatok - 10); // новое значение второй с конца цифры
+
+                if (Big_int.get_size() > 2) { // если число больше 99
+                    for (int i = 2; i < Big_int.get_size(); i++) { // проход по числу, пока переход в следующие разряды не прекратится
+                        next_node->next;
+                        if (next_node->value == 9) {
+                            next_node->value = 0;
+                            continue;
+                        }
+                        next_node->value += 1;
+                        break;
+                    }
+                    if (next_node->value == 0) { // если разряд числа увеличивается
+                        Big_int.push_back(1);
+                    }
+                }
+                else { // двузначному числу добавляется разряд сотен
+                    Big_int.push_front(1);
+                }
+            }
+            else { // двузначное число не меняет своей разрядности
+                next_node->value += desatok;
+            }
+        }
+        else { // одозначное число становится двузначным
+            Big_int.push_back(desatok);
+        }
+    }       
+    else {
+        Big_int.get_first()->value *= c;
+    }
+}
+
+NaturalNumber& NaturalNumber::SUB_NN_N(const NaturalNumber& num)
+{
+    int size1 = Big_int.get_size(); // размер первого числа
+    int size2 = num.Big_int.get_size(); // размер второго числа
+
+    int point = 0; // если вычитание выйдет за пределы одного разряда, придётся взять одну цифру из следущего
+    int i = 1;
+
+    int num1 = Big_int.get_first()->value; //значение первого разряда первого числа
+    int num2 = num.Big_int.get_first()->value; //значение первого разряда второго числа
+
+    if (num1 < num2) { //если значение первого разряда второго числа больше первого
+        num1 += 10; //разряд первого числа получает 10 от следущего
+        num1 -= num2; //вычитание разрядов
+        point = 1; //будет учитываться в дальнейшем вычитании за взятую ранее десятку
+    }
+    else {
+        num1 -= num2; //если значение первого разряда первого числа равно или больше
+        point = 0;
+    }
+
+    Big_int.get_first()->value = num1; //полученная разность
+
+    Node* next_node1 = Big_int.get_first()->next; //сдвиг на следующий разряд первого числа
+    Node* next_node2 = num.Big_int.get_first()->next;//сдвиг на следующий разряд второго числа
+
+    while (i < size2) {
+
+        next_node1 = next_node1->next;//сдвиг на следующий разряд первого числа
+        next_node2 = next_node2->next; //сдвиг на следующий разряд второго числа
+
+        num1 = next_node1->value; //значение i-го разряда первого числа
+        num2 = next_node2->value; //значение i-го разряда второго числа
+
+        if (num1 < num2) { //если значение i-го разряда второго числа больше первого
+            num1 += 10; //разряд первого числа получает 10 от следущего
+            num1 -= num2; //вычитание разрядов
+            point = 1; //будет учитываться в дальнейшем вычитании за взятую ранее десятку
+        }
+        else {
+            num1 -= num2; //если значение i-го разряда первого числа равно или больше
+            point = 0;
+        }
+
+        next_node1->value = num1; //полученная разность
+
+        i++;
+    }
+    next_node1 = next_node1->next;//сдвиг на следующий разряд первого числа
+    if ((point == 1) && (next_node1->value == 0)) {//если осталась единица, которую надо вычесть из старших разрядов, но там 0
+        while (next_node1->value != 0) { //поиск, пока не наткнёмся на отличное от 0 число
+            next_node1->value = 9;
+            next_node1->next;
+        }
+        point = 0;
+        next_node1->value -= 1; //вычитание единицы
+    }
+    if (point == 1) {//если осталась единица, которую надо вычесть из старших разрядов, при этом там не 0
+        next_node1->value -= 1;
+    }
+    //Node* check_node = Big_int.get_last();
+    //for (int j = 0; j < size1; j++) {
+    //    if (check_node->value != 0) {
+    //       break;                         ////удаление незначащих нулей слева направо, надо добавить prev
+    //   }
+    //   if (check_node->value == 0) {
+    //        check_node->prev = nullptr;
+      //  }
+    //}
+}
+    
+NaturalNumber& NaturalNumber::MUL_Nk_N(const NaturalNumber& k)
+{
+    if ((Big_int.get_size() != 1) && (Big_int.get_first()->value != 0)) {
+        if (k.Big_int.get_first()->value != 1) {
+            for (int i = 0; i < k.Big_int.get_size(); i++) {
+                Big_int.push_front(0);
+            }
+        }
+    }
+}
--- tests/NaturalNumberTest.cpp
+++ tests/NaturalNumberTest.cpp
@@ -1,3 +1,163 @@
-//
-// Created by nikit on 29.10.2024.
-//
+#include <gtest/gtest.h>
+#include "NaturalNumber.h"
+
+TEST(NaturalNumberTests, NZER_N_B) {
+    //реяр 1: ярюмдюпрмши реяр дкъ мемскебнцн вхякю
+    NaturalNumber n1;
+    n1.add(12345);
+    EXPECT_TRUE(n1.NZER_N_B(), true);
+
+    //реяр 2: ярюмдюпрмши реяр дкъ мскебнцн вхякю
+    NaturalNumber n1;
+    n1.add(0);
+    EXPECT_TRUE(n1.NZER_N_B(), false);
+}
+
+TEST(NaturalNumberTests, COM_NN_D) {
+    //реяр 1: ярюмдюпрмши реяр дкъ дбсу вхяек я ндхмюйнбни пюгпъдмнярэч, цде брнпне анкэье оепбнцн
+    NaturalNumber n1, n2, n3;
+    n1.add(100);
+    n2.add(300);
+    EXPECT_EQ(n1.COM_NN_D(n2),1);
+
+    //реяр 2: ярюмдюпрмши реяр дкъ дбсу вхяек я ндхмюйнбни пюгпъдмнярэч, цде оепбне анкэье брнпнцн
+    NaturalNumber n1, n2, n3;
+    n1.add(478);
+    n2.add(420);
+    EXPECT_EQ(n1.COM_NN_D(n2), 2);
+
+    //реяр 3: ярюмдюпрмши реяр дкъ дбсу пюбмшу вхяек я ндхмюйнбни пюгпъдмнярэч
+    NaturalNumber n1, n2, n3;
+    n1.add(666000);
+    n2.add(666000);
+    EXPECT_EQ(n1.COM_NN_D(n2), 0);
+
+    //реяр 4: ярюмдюпрмши реяр дкъ дбсу вхяек я пюгмни пюгпъдмнярэч, цде оепбне анкэье брнпнцн
+    NaturalNumber n1, n2, n3;
+    n1.add(12456666599);
+    n2.add(666599);
+    EXPECT_EQ(n1.COM_NN_D(n2), 2);
+
+    //реяр 4: ярюмдюпрмши реяр дкъ дбсу вхяек я пюгмни пюгпъдмнярэч, цде брнпне анкэье оепбнцн
+    NaturalNumber n1, n2, n3;
+    n1.add(9);
+    n2.add(35409);
+    EXPECT_EQ(n1.COM_NN_D(n2), 1);
+}
+
+TEST(NaturalNumberTests, ADD_1N_N) {
+    ///реяр 1: ярюмдюпрмши реяр дкъ вхякю ян ялемни пюгпъдмнярх 
+    NaturalNumber n;
+    n.add(999);
+    n.ADD_1N_N();
+    EXPECT_EQ(n.get_value(), 10000);
+
+    ///реяр 1: ярюмдюпрмши реяр дкъ вхякю аег ялемш пюгпъдмнярх 
+    NaturalNumber n;
+    n.add(777);
+    n.ADD_1N_N();
+    EXPECT_EQ(n.get_value(), 778);
+}
+
+TEST(NaturalNumberTests, ADD_NN_N) {
+    ///реяр 1: ярюмдюпрмши реяр дкъ дбсу вхяек аег ялемш пюгпъдмнярх
+    NaturalNumber n1, n2, result;
+    n1.add(123);
+    n2.add(456);
+    result.ADD_NN_N(n2);
+    EXPECT_EQ(result.get_value(), 579);
+
+    ///реяр 1: ярюмдюпрмши реяр дкъ дбсу вхяек ян ялемни пюгпъдмнярх
+    NaturalNumber n1, n2, result;
+    n1.add(4567356);
+    n2.add(8955787);
+    result.ADD_NN_N(n2);
+    EXPECT_EQ(result.get_value(), 579);
+}
+
+TEST(NaturalNumberTests, MUL_ND_N) {
+    //реяр 1: ярюмдюпрмши яксвюи дкъ дбсгмювмнцн вхякю аег ялемш пюгпъдмнярх
+    NaturalNumber n;
+    n.add(12);
+    n.MUL_ND_N(3);
+    EXPECT_EQ(n.get_value(), 36);
+
+    //реяр 2: ярюмдюпрмши яксвюи дкъ дбсгмювмнцн вхякю ян ялемни пюгпъдмнярх
+    NaturalNumber n;
+    n.add(35);
+    n.MUL_ND_N(4);
+    EXPECT_EQ(n.get_value(), 140);
+
+    //реяр 3: ярюмдюпрмши яксвюи дкъ ндмнгмювмнцн вхякю ян ялемни пюгпъдмнярх
+    NaturalNumber n;
+    n.add(3);
+    n.MUL_ND_N(4);
+    EXPECT_EQ(n.get_value(), 12);
+
+    //реяр 4: ярюмдюпрмши яксвюи дкъ ндмнгмювмнцн вхякю аег ялемш пюгпъдмнярх
+    NaturalNumber n;
+    n.add(3);
+    n.MUL_ND_N(3);
+    EXPECT_EQ(n.get_value(), 9);
+
+    //реяр 5: слмнфемхе мю 0
+    NaturalNumber n;
+    n.add(12);
+    n.MUL_ND_N(0);
+    EXPECT_EQ(n.get_value(), 0);
+}
+
+TEST(NaturalNumberTests, SUB_NN_N) {
+    ///реяр 1: ярюмдюпрмши яксвюи дкъ пюгмшу вхяек аег ялемш пюгпъдмнярх
+    NaturalNumber n1, n2, result;
+    n1.add(60);
+    n2.add(50);
+    result.SUB_NN_N(n2);
+    EXPECT_EQ(result.get_value(), 10);
+
+    ///реяр 2: ярюмдюпрмши яксвюи дкъ пюгмшу вхяек ян ялемни пюгпъдмнярх
+    NaturalNumber n1, n2, result;
+    n1.add(160);
+    n2.add(85);
+    result.SUB_NN_N(n2);
+    EXPECT_EQ(result.get_value(), 75);
+
+    ///реяр 3: ярюмдюпрмши яксвюи дкъ пюгмшу вхяек ян ялемни пюгпъдмнярх
+    NaturalNumber n1, n2, result;
+    n1.add(160);
+    n2.add(85);
+    result.SUB_NN_N(n2);
+    EXPECT_EQ(result.get_value(), 75);
+
+    ///реяр 3: ярюмдюпрмши яксвюи дкъ пюгмшу вхяек ян ялемни пюгпъдмнярх
+    NaturalNumber n1, n2, result;
+    n1.add(160);
+    n2.add(85);
+    result.SUB_NN_N(n2);
+    EXPECT_EQ(result.get_value(), 75);
+}
+
+TEST(NaturalNumberTests, MUL_Nk_N) {
+    //реяр 1: ярюмдюпрмши реяр дкъ мемскебнцн вхякю х 10^K, цде K > 0
+    NaturalNumber n1, k;
+    n1.add(32);
+    k.add(100);
+    EXPECT_TRUE(n1.MUL_Nk_N(k), 3200);
+
+    //реяр 2: ярюмдюпрмши реяр дкъ мскебнцн вхякю
+    NaturalNumber n1, k;
+    n1.add(0);
+    k.add(100000000000);
+    EXPECT_TRUE(n1.MUL_Nk_N(k), 0);
+
+    //реяр 3: ярюмдюпрмши реяр дкъ мемскебнцн вхякю х 10^K, цде K = 0
+    NaturalNumber n1, k;
+    n1.add(6987);
+    k.add(1);
+    EXPECT_TRUE(n1.MUL_Nk_N(k), 6987);
+}
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
