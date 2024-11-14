62f9c99734c9ef0d5e08ccf3610a77e55aafb6be
NiksBrn <niks_kam@vk.com>
2024-11-15 01:52:50 +0300

    Our minds sometimes fail us

--- sources/modules/Polynomial.cpp
+++ sources/modules/Polynomial.cpp
@@ -49,6 +49,192 @@ ListPolynomial &Polynomial::getPolynomial() const{
     return *polynomial;
 }
 
+Polynomial Polynomial::ADD_PP_P(const Polynomial& other) {
+    ListPolynomial* newPol = new ListPolynomial();
+    NodePolynomial* tmp1 = polynomial->get_tail();
+    NodePolynomial* tmp2 = other.polynomial->get_tail();
+    while (tmp1 != nullptr && tmp2 != nullptr) {
+        if (tmp1->degree == tmp2->degree) {
+            newPol->push(tmp1->coefficient.ADD_QQ_Q(tmp2->coefficient), tmp1->degree);
+        } else {
+            newPol->push(tmp1->coefficient, tmp1->degree);
+            newPol->push(tmp2->coefficient, tmp2->degree);
+        }
+        tmp1 = tmp1->prev;
+        tmp2 = tmp2->prev;
+    }
+    while (tmp1 != nullptr) {
+        newPol->push(tmp1->coefficient, tmp1->degree);
+        tmp1 = tmp1->prev;
+    }
+    while (tmp2 != nullptr) {
+        newPol->push(tmp2->coefficient, tmp2->degree);
+        tmp2 = tmp2->prev;
+    }
+    Polynomial newPolynomial(newPol);
+    newPolynomial.polynomial->balance();
+    return newPolynomial;
+}
+
+Polynomial Polynomial::SUB_PP_P(const Polynomial& other) {
+    ListPolynomial* newPol = new ListPolynomial();
+    NodePolynomial* tmp1 = polynomial->get_tail();
+    NodePolynomial* tmp2 = other.polynomial->get_tail();
+    RationalNumber reverse('-', "1", "1");
+    while (tmp1 != nullptr && tmp2 != nullptr) {
+        if (tmp1->degree == tmp2->degree) {
+            newPol->push(tmp1->coefficient.SUB_QQ_Q(tmp2->coefficient), tmp1->degree);
+        } else {
+            // auto neg_coeff1 = tmp1->coefficient.MUL_QQ_Q(reverse);
+            newPol->push(tmp1->coefficient, tmp1->degree);
+            newPol->push(tmp2->coefficient.MUL_QQ_Q(reverse), tmp2->degree);
+        }
+        tmp1 = tmp1->prev;
+        tmp2 = tmp2->prev;
+    }
+    while (tmp1 != nullptr) {
+        newPol->push(tmp1->coefficient, tmp1->degree);
+        tmp1 = tmp1->prev;
+    }
+    while (tmp2 != nullptr) {
+        newPol->push(tmp2->coefficient.MUL_QQ_Q(reverse), tmp2->degree);
+        tmp2 = tmp2->prev;
+    }
+    Polynomial newPolynomial(newPol);
+    newPolynomial.polynomial->balance();
+    return newPolynomial;
+}
+
+Polynomial Polynomial::MUL_PQ_P(const RationalNumber& other) {
+    ListPolynomial* newPol = new ListPolynomial();
+    NodePolynomial* tmp = polynomial->get_tail();
+    while (tmp != nullptr) {
+        newPol->push(tmp->coefficient.MUL_QQ_Q((RationalNumber&)other),
+                     tmp->degree);
+        tmp = tmp->prev;
+    }
+    Polynomial newPolynomial(newPol);
+    newPolynomial.polynomial->balance();
+    return newPolynomial;
+}
+
+Polynomial Polynomial::MUL_Pxk_P(const NaturalNumber& other) {
+    ListPolynomial* newPol = new ListPolynomial();
+    NodePolynomial* tmp = polynomial->get_tail();
+    while (tmp != nullptr) {
+        newPol->push(tmp->coefficient, tmp->degree.ADD_NN_N(other));
+        tmp = tmp->prev;
+    }
+    Polynomial newPolynomial(newPol);
+    newPolynomial.polynomial->balance();
+    return newPolynomial;
+}
+
+RationalNumber Polynomial::LED_P_Q() {
+    NodePolynomial* tmp = polynomial->get_head();
+    return tmp->coefficient;
+}
+
+RationalNumber Polynomial::FAC_P_Q() {
+    NodePolynomial* tmp = polynomial->get_tail();
+    NaturalNumber nok("1");
+    NaturalNumber nod("1");
+    if (tmp != nullptr) {
+        nok = tmp->coefficient.getDenominator();
+        nod = tmp->coefficient.getNumerator().ABS_Z_Z();
+        tmp = tmp->prev;
+    }
+    while (tmp != nullptr) {
+        nok = tmp->coefficient.getDenominator().LCM_NN_N(nok);
+        nod = tmp->coefficient.getNumerator().ABS_Z_Z().GCF_NN_N(nod);
+        tmp = tmp->prev;
+    }
+    IntegerNumber nok_i(nok);
+    RationalNumber answer(nok_i, nod);
+    return answer;
+}
+
+Polynomial Polynomial::DER_P_P() {
+    ListPolynomial* newPol = new ListPolynomial();
+    RationalNumber k('+', "0", "1");
+    NodePolynomial* tmp = polynomial->get_tail();
+    while (tmp != nullptr) {
+        if (tmp->degree.COM_NN_D(NaturalNumber("0")) == Compare::EQUAL) {
+            tmp = tmp->prev;
+            continue;
+        }
+        k = RationalNumber(IntegerNumber(tmp->degree), NaturalNumber("1"));
+        newPol->push(tmp->coefficient.MUL_QQ_Q(k),
+                     tmp->degree.SUB_NN_N(NaturalNumber("1")));
+        tmp = tmp->prev;
+    }
+    Polynomial newPolynomial(newPol);
+    newPolynomial.polynomial->balance();
+    return newPolynomial;
+}
+
+Polynomial Polynomial::DIV_PP_P(const Polynomial& other) {
+    ListPolynomial* object = new ListPolynomial();
+    Polynomial temp_poly = *this;
+    Polynomial temp_other = other;
+    NodePolynomial* poly = temp_poly.polynomial->get_head();
+    NaturalNumber maxDegree = temp_other.polynomial->get_head()->degree;
+    RationalNumber coefficient_poly =
+            temp_other.polynomial->get_head()->coefficient;
+    // пока полином существует и пока его можно поделить
+    while (poly != nullptr && poly->degree.COM_NN_D(maxDegree) != Compare::LESS) {
+        // получение степени
+        NaturalNumber degree = poly->degree.SUB_NN_N(maxDegree);
+        // получение коэффициента
+        RationalNumber coefficient = poly->coefficient.DIV_QQ_Q(coefficient_poly);
+        // добалвение монома в полином
+        object->push(coefficient, degree);
+        // вычитание из делимого делителя, умноженного на моном
+        temp_poly =
+                temp_poly.SUB_PP_P(temp_other.MUL_PQ_P(coefficient).MUL_Pxk_P(degree));
+        poly = temp_poly.polynomial->get_head();
+    }
+    // если цикл ни разу не сработал
+    if (poly == polynomial->get_head()) {
+        object->push(RationalNumber(IntegerNumber('+', "0")), NaturalNumber("0"));
+    }
+    return Polynomial(object);
+}
+
+Polynomial Polynomial::NMR_P_P() {
+    Polynomial Der = (*this).DER_P_P();  // Производная многочлена
+    Polynomial GCF = (*this).GCF_PP_P(Der);  // НОД многочлена
+    Polynomial NMR = *this;
+    if (!(GCF.DEG_P_N() == NaturalNumber("0")) ||
+        !(GCF.polynomial->get_tail()->coefficient.Trans_Q_Z().POZ_Z_D() ==
+          ZERO)) {
+        NMR = (*this).DIV_PP_P(GCF);
+    } else {
+        NMR = GCF;
+    }
+    return NMR;
+}
+
+Polynomial Polynomial::MUL_PP_P(const Polynomial& other) {
+    NodePolynomial* tmp = other.polynomial->get_head();
+    ListPolynomial* newPol = new ListPolynomial();
+    newPol->push(RationalNumber('+', "0", "1"), NaturalNumber("0"));
+    Polynomial poly(newPol);
+    Polynomial copy = *this;
+    while (tmp != nullptr) {
+        Polynomial mul = copy.MUL_PQ_P(tmp->coefficient).MUL_Pxk_P(tmp->degree);
+        poly = poly.ADD_PP_P(mul);
+        tmp = tmp->next;
+    }
+    poly.polynomial->balance();
+    return poly;
+}
+
+
+
+
+
+
 Polynomial Polynomial::MOD_PP_P(const Polynomial &other) {
     Polynomial MOD = (*this).DIV_PP_P(other);
     Polynomial ans = (*this).SUB_PP_P(MOD.MUL_PP_P(other));
